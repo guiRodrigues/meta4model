2de3158196790b34486cd7f129844daa423ff8dc
Felipe Lima <lipezera1836@gmail.com>
2025-06-26 00:40:38 -0300

    feat(bpmn): BPMN model added to homepage

---
+++ models/bpmn-model.tsx
@@ -0,0 +1,23 @@
+import type { ModelDefinition } from "@/types/model-types"
+
+export const bpmnModel: ModelDefinition = {
+    id: "bpmn",
+    name: "BPMN",
+    description: "Business Process Model and Notation - A standard for business process modeling",
+    thumbnail: "/models/bpmn-placeholder.png",
+
+    nodeTypes: {
+    },
+
+    edgeTypes: {
+    },
+
+    defaultNodes: [
+    ],
+
+    defaultConnections: [
+
+    ],
+
+
+}
