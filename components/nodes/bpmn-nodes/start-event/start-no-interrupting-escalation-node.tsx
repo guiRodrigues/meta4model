e32effa31cbdbd9821a9bf9b9e678abc9cf6f82c
Edivar Cruz <edivarfilho16@gmail.com>
2025-07-10 13:09:42 -0300

    hasParent adicionado e top-12

--- components/nodes/bpmn-nodes/start-event/start-no-interrupting-escalation-node.tsx
+++ components/nodes/bpmn-nodes/start-event/start-no-interrupting-escalation-node.tsx
@@ -3,15 +3,24 @@
 import type React from "react";
 
 import { memo, useState, useEffect, useRef } from "react";
-import { Handle, Position, type NodeProps } from "reactflow";
+import { Handle, Position, useReactFlow, useStore, type NodeProps } from "reactflow";
 import type { NodeData } from "@/types/kaos-types";
 import {CircleNoInterruptingEscalation,} from "@/components/icons/bpmn-icons";
+import useDetachNodes from "@/hooks/use-detach-nodes";
 
 function StartNoInterruptingEscalation({ data, type, id }: NodeProps<NodeData>) {
   const [isEditing, setIsEditing] = useState(false);
   const [label, setLabel] = useState(data.label);
   const inputRef = useRef<HTMLInputElement>(null);
+  const hasParent = useStore((store) => {
+    const node = store.getNodes().find((n) => n.id === id);
+    return !!node?.parentId;
+  });
+  const { deleteElements } = useReactFlow();
+  const detachNodes = useDetachNodes();
 
+  const onDelete = () => deleteElements({ nodes: [{ id }] });
+  const onDetach = () => detachNodes([id]);
   // Use our fixed node size
   const size = 60;
 
@@ -86,7 +95,7 @@ function StartNoInterruptingEscalation({ data, type, id }: NodeProps<NodeData>) 
           </div>
         ) : (
           <div
-            className="text-xs font-medium text-gray-700 text-center absolute top-full mt-1 left-1/2 transform -translate-x-1/2 break-words whitespace-normal"
+            className="text-xs font-medium text-gray-700 text-center absolute top-12 mt-1 left-1/2 transform -translate-x-1/2 break-words whitespace-normal"
             style={{ maxWidth: "160px", width: "max-content" }}
           >
             {data.label}
